bbce9dac298ff062571b44626101c824fd9fd1c2
Ghana Dahal <ghana.dahal@my.tccd.edu>
2024-10-07 21:23:37 -0500

    breakpoint function

--- classwork/Chapter 6/MovieLibrary/MovieLibrary.cpp
+++ classwork/Chapter 6/MovieLibrary/MovieLibrary.cpp
@@ -36,52 +36,46 @@ enum MenuCommand
     //MC_End
 
 };
+// function defination -defines functions what it does
+//declaration ::= declare the existence of something and what it is (eg; var declaration) Example- MenuCommand menuCommand = (MenuCommand)0;
+//              used by the complier to recongnize tohe use of the identifier( for complier)
+//              must be declared before usage
+// definition ::= what it does (var defined), function body inside the curly braces ( for linker)
+//Function names are verbs representing actions,
+//Casing: Pascal casing, camel casing 
 
-
-int main()
+/// Display main menu
+void DisplayMenu()
 {
-    //Nested loop menu
-    int iterations = 0;
-    for (int i = 0;i < 100;++i)
-    {
-       /* if (i != 0)
-        {
-            char choice;
-            cout << "Do you want to give up?";
-            cin >> choice;
-            if (choice == 'Y' || choice == 'y')
-                break;
-        };*/
-        for (int j = 0;j < 10;++j)
-        {
-            if (iterations % 100 == 0)
-            {
-                char choice;
-                cout << "Do you want to give up?";
-                cin >> choice;
-                if (choice == 'Y' || choice == 'y')
-                    break;
-            };
-            ++iterations;
-            cout << i << ", " << j << " = " << iterations << endl;
-        };
-    };
-    cout << "Total iteraitons = " << iterations << endl;
-  
-
-    //Show menu
     cout << "Movie Library" << endl;
     cout << "______________" << endl;
     cout << "A)dd Movie" << endl;
     cout << "E)dit Movie" << endl;
     cout << "D)elete Movie" << endl;
     cout << "V)iew Movie" << endl;
-
-    //While-stmt ::= while (Eb) S;
-    // Get input
+}
+/// Handles the menu selection
+void HandleMenu()
+{
+    //HACK: Fix this 
     MenuCommand menuCommand = (MenuCommand)0;
-   // bool done = false;
-    //while(!done)
+    switch (menuCommand)
+    {
+        case MenuCommand::AddMovie:
+        case MenuCommand::EditMovie:
+        case MenuCommand::DeleteMovie:
+        case MenuCommand::ViewMovie: cout << "Not implemented" << endl;break;
+    };
+}
+
+int main()
+{
+    //Function call ::=id();
+    DisplayMenu();
+    
+    //// Get input
+    MenuCommand menuCommand = (MenuCommand)0;
+   
     do 
     {
         char input;
@@ -107,20 +101,16 @@ int main()
         };
     } while (menuCommand == 0);
     cin.ignore();
-   // int shouldntWork = MenuCommand::MC_AddMovie;
-    //menuCommand = (MenuCommand)1;
     
-    
-    //Handle menu Command
-    switch (menuCommand)
+    ////Handle menu Command
+    HandleMenu();
+    /*switch (menuCommand)
     {
         case MenuCommand::AddMovie:
         case MenuCommand::EditMovie:
         case MenuCommand::DeleteMovie:
         case MenuCommand::ViewMovie: cout << "Not implemented" << endl;break;
-    };
-
-
+    };*/
     //Add a new movie
     //Create a new Movie
     Movie movie; // { 0 };
@@ -130,16 +120,12 @@ int main()
     do
     {
         cout << "Enter a title: ";
-       // cin >> movie.Title;
+   
         getline(cin, movie.Title);
-        //Decisio making -IF statement
-        //If-ststement ::= if (Eb) S
+       
         if (movie.Title == "")
 
-            cout << "ERROR: Title is required" << endl;
-
-           
-
+            cout << "ERROR: Title is required" << endl;       
     } while (movie.Title == "");
     //Get run length, at least 0. minutes
     //movie.RunLength = -1;
@@ -148,24 +134,7 @@ int main()
         cout << " Enter run length( in Minutes):";
         cin >> movie.RunLength;
 
-       // if (movie.RunLength < 0)
-        //{
-        //    //Look at additinal stuff .....
-        //    if (movie.RunLength < 0)
-        //    {
-        //        cout << "ERROR: Run length must be at least 0" << endl;
-
-        //        cout << "Enter run length (in minutes): ";
-        //        cin >> movie.RunLength;
-        //    };
-        //}
-        // else if (movie.RunLength > 1440)
-        //{
-        //    cout << "ERROR: Run length cannot be greater than a day " << endl;
-
-        //    cout << "Enter run length (in minutes): ";
-        //    cin >> movie.RunLength;
-        //};
+       
         if (movie.RunLength < 0 || movie.RunLength > 1440)
         {
             cout << "ERROR: Run length must be between 0 and 1440. " << endl;
@@ -185,9 +154,6 @@ int main()
         if (movie.ReleaseYear < 1900 || movie.ReleaseYear > 2100)
         {
             cout << "ERROR: Release Year must be between 1900 and 2100" << endl;
-
-           // cout << "Enter Release Year (1900+): ";
-            //cin >> movie.ReleaseYear;
         };
     };
     // Get the Optinal Description
@@ -234,9 +200,6 @@ int main()
         movie.Genre += genre + ", ";
 
     };
-
-   
-
     cout << "-------------------" << endl;
     cout << movie.Title << "(" << movie.ReleaseYear << ")" << endl;
     cout << "Run Length (mins) " << movie.RunLength << endl;
@@ -251,4 +214,5 @@ int main()
 
     cout << "-------------------" << endl;
 }
+
 
