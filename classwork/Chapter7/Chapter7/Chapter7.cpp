ce2d025726c5b706d4f78598f42a8c7c7cf61010
Ghana Dahal <ghana.dahal@my.tccd.edu>
2024-10-30 21:25:07 -0500

    class work

--- classwork/Chapter7/Chapter7/Chapter7.cpp
+++ classwork/Chapter7/Chapter7/Chapter7.cpp
@@ -127,6 +127,38 @@ void InitArrayDemo()
         std::cout << monthNames[index] << " has " << daysInMonth[index] << "days" << std::endl;
 }
 
+
+// Function parameters 'T id[]'
+// Always open array, no size
+// Size is always needed as param when using arrays
+bool AreArrayEqual(int left[] , int leftSize, int right[], int rightSize)// allow any size for array parameter
+{
+    if (leftSize != rightSize)
+        return false;
+
+    for (int index = 0; index < leftSize; ++index)
+        if (left[index] != right[index])
+            return false;
+    return true;
+};
+
+int CopyArray(int left[], int leftSize, int right[], int rightSize)
+{
+    int size = (leftSize < rightSize) ? leftSize : rightSize;
+    for (int index = 0; index < size; ++index)
+        left[index] = right[index];
+
+    return size;
+
+};
+
+//Array cannot be return type from function
+void InitializeArray( int values[], int size, int initialValue)
+{
+    for (int index = 0; index < size; ++index)
+        values[index] = initialValue;
+};
+
 //use array condition
 void useArrayDemo()
 {
@@ -135,9 +167,9 @@ void useArrayDemo()
     //for (int index = 0; index < 20; ++index)
     //    array1[index] = index + 1;
     //--- modify to pass by parameter alternatively above stick with the above when you modify the index
-    for (int& value : array1)
-        value = 1;
-
+  /*  for (int& value : array1)
+        value = 1;*/
+    InitializeArray(array1, 20, 2);// there is no passby value even it look like , it always modify the element of the array not the modify the array so it pass by refrence.
 
     //int index2 = 0;
     //while (index2 < 20)
@@ -160,13 +192,26 @@ void useArrayDemo()
     for (int value : array1)
         std::cout << value << std::endl;
 
+    int array2[20] = {0};
+    int array3[20] = {0};
+    //int array2[19] = 10;
+
+    //Compare arrays
+   //std::cout << (array2 == array3) << std::endl;// ** Very IMPORTANT fail because it is needed the size of the array because of the elemnt at run time but still compile.
+   std::cout << (array2 == array3) << " " << AreArrayEqual(array2, 20, array3, 20) << std::endl;
+
+
+   // Assignment
+   //array3 = array2;
+   //to copy array we write function
+   CopyArray(array3, 20, array2, 20);
 }
 
 int main()
 {
    
-    InitArrayDemo();
-    
+    //InitArrayDemo();
+    useArrayDemo();
     
     //double gradeAverages[100];
    // GradeArrayDemo();
