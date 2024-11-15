6fe0e0e43cadcb35dfa990c5dd621d4d0e2cac0b
Ghana Dahal <ghana.dahal@my.tccd.edu>
2024-11-15 17:36:14 -0600

    Lab 4

--- labs/Lab4/Lab4/Lab4.cpp
+++ labs/Lab4/Lab4/Lab4.cpp
@@ -10,16 +10,16 @@ using namespace std;
 
 /// @brief function is designed to prompt the user to input numbers, storing them in an array. 
 /// It ensures that the number of inputs does not exceed the specified maximum size of the array.
-/// @param numbers;an array of integers where the function will store the numbers input by the user. 
+/// @param numbers[];an array of integers where the function will store the numbers input by the user. 
 /// @param maxNumbersSize ; an integer where the maximum size of the numbers array can store.
 /// @param startCount; indicates the number of elements present, in the array before the function starts at 0 adding new numbers.
 /// @return count;the total number of numbers successfully stored in the array after the function completes. 
 /// This includes any pre-existing numbers plus the new ones added by the function.
-int promptForNumbers(int numbers[], int maxNumbersSize, int startCount = 0) 
+int promptForNumbers(int numbers[], int maximumNumbersSize, int startCount = 0) 
 {
     int count = startCount;// initial counter for the number of valid entries
     int value;
-    while(count < maxNumbersSize) 
+    while(count < maximumNumbersSize) 
     {
         cout << "Enter an integral number (0 to stop): ";
         if (cin >> value)
@@ -39,16 +39,20 @@ int promptForNumbers(int numbers[], int maxNumbersSize, int startCount = 0)
         else
         {
                 cin.clear(); // clear the error 
-                cin.ignore(maxNumbersSize, '\n'); // discard invalid input and space
+                cin.ignore(maximumNumbersSize, '\n'); // discard invalid input and space
                 cout << "Error: Please enter a valid integral number." << endl;
                 continue;
         }
     }
+    if (count >= maximumNumbersSize)// checking the limit.
+    {
+        cout << "You exceed the entering number limit." << endl;
+    }
     return count;// Return the number of valid entries
 }
 
-/// @brief; function to display the main menu and get the user's choice 
-/// @return; The function returns the user's choice as an uppercase character.
+/// @brief;function to display the main menu and get the user's choice 
+/// @return;the function returns the user's choice as an uppercase character.
 char displayMenu() 
 {
     char choice;
@@ -64,7 +68,7 @@ char displayMenu()
     cout << setw(15) << setfill('-') << "" << setfill(' ') << endl;
     cout << "Select an option or Q for Exit the Program: ";
     cin >> choice;
-    return toupper(choice);// Convert choice to uppercase to handle case insensitivity
+    return toupper(choice);// convert choice to uppercase to handle case insensitivity
 }
 
 /// @brief; the function getLargestValue() finds and returns the largest value in an array of integers.
@@ -115,15 +119,20 @@ int getSumOfValues(const int numbers[], int count)
     return sum;
 }
 
-// Function to get the mean of values in the array
-//Returns the mean value.
+/// @brief;function getMeanOfValues() calculates and returns the mean (average) of all elements in an array.
+/// @param const int numbers[];const type integer indicates that the function will not modify the array elements. 
+/// @param count; shows the number of elements in the array. 
+/// @return a double,the mean of the values in the array. 
 double getMeanOfValues(const int numbers[], int count) 
 {
     return static_cast<double>(getSumOfValues(numbers, count)) / count;
     //dividing the sum of the array elements that is converted to double/the number of elements.
 }
 
-// Function to display the values in the array,showing 10 values per line.
+/// @brief;function displayValues() prints all elements of an array of integers. 
+/// Which displaying up to 10 values per line for better readability.
+/// @param const int numbers[]; const type int indicates,the function will not modify the array elements.  
+/// @param count;counts the number of elements in the array.
 void displayValues(const int numbers[], int count) 
 {
     for (int i = 0; i < count; ++i) 
@@ -136,22 +145,30 @@ void displayValues(const int numbers[], int count)
     }
     cout << endl;
 }
-/// @brief Function to add more values to the array
-/// @param numbers 
-/// @param count 
-/// @param maxNumbersSize 
-/// @return 
-int addMoreValues(int numbers[], int count, int& maxNumbersSize)
+/// @brief;function addMoreValues() add more values to an existing array of integers
+/// which by calling another function, promptForNumbers().
+/// @param int numbers[];an array of integers where new values will be added.
+/// @param count; count the current number of elements in the array. 
+/// @param maxNumbersSize; a reference to an integer representing the maximum size of the array. 
+/// reference allows the function to modify the original variable when needed.
+/// @return; an integer, which is likely the new count of elements in the array after adding more values.
+int addMoreValues(int numbers[], int count, int& maximumNumbersSize)
 {
-    return promptForNumbers(numbers, maxNumbersSize, count);
+    if (count >= maximumNumbersSize)
+    {
+        cout << "You cannot add more than 100 values. The maximun number reach the limit." << endl;
+        return count; // Return the current count without adding new values
+    }
+    return promptForNumbers(numbers, maximumNumbersSize, count);
 }
 
 /// @brief Function to handle the menu choices takes the user's choice.
-/// @param choice 
-/// @param numbers 
-/// @param count 
-/// @param maxSize 
-void handleMenuFunction(char choice, int numbers[], int& count, int maxSize) 
+/// @param choice;user's menu choice which determines the action to be performed. 
+/// @param numbers[];an array of integers that stores the values to be processed.
+/// @param int& count;A reference to an integer representing the current number of elements in the array. 
+/// This allows the function to update the count if more values are added. 
+/// @param maxValuesSize;The maximum size,how many values can be stored.
+void handleMenuFunction(char choice, int numbers[], int& count, int maximumValuesSize) 
 {
     switch (choice) 
     {
@@ -161,7 +178,7 @@ void handleMenuFunction(char choice, int numbers[], int& count, int maxSize)
             cout << setw(15) << setfill('-') << "" << setfill(' ') << endl;
             break;
         case 'A':
-            count = addMoreValues(numbers, count, maxSize);
+            count = addMoreValues(numbers, count, maximumValuesSize);
             break;
         case 'L':
                 cout << "Largest value: " << getLargestValue(numbers, count) << endl;
@@ -188,11 +205,11 @@ void handleMenuFunction(char choice, int numbers[], int& count, int maxSize)
     }
 }
 
-/// @brief Main header file and entry of the program starting point.
-/// @return choice with detail of the choice result.
+/// @brief; main header with programing title and course info,and also the starting point of the program.
+/// @return; function returns an integer with detail of the choice result.
 int main() 
 {
-    //Display Program Title, My Name, Course and Semester
+    //Display Basic Program Title,Name, Course and Semester
     cout << setw(25) << setfill('*') << "" << setfill(' ') << endl;
     cout << " Program title:Lab 4 \n";
     cout << " Ghana Dahal \n";
@@ -202,13 +219,13 @@ int main()
 
     //Diplaying message for program starting point
     cout << "Welcome to the Number Management Program!" << endl;
-    const int maxNumberSize = 100;// Maximum number of integers that can be stored
-    int numbers[maxNumberSize]; // Array to store the numbers
-    int count = promptForNumbers(numbers, maxNumberSize); // Get initial numbers from the user
+    const int maximumNumberSize = 100;// Maximum number of integers that can be stored
+    int numbers[maximumNumberSize]; // Array to store the numbers
+    int count = promptForNumbers(numbers, maximumNumberSize); // Get initial numbers from the user
     while (true) 
     {
         char choice = displayMenu();// Display menu and get user's choice
-        handleMenuFunction(choice, numbers, count, maxNumberSize);// Handle the menu choice
+        handleMenuFunction(choice, numbers, count, maximumNumberSize);// Handle the menu choice
     }
 }
 
